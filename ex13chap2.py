29b88b3fd2d4629ccdf11fcdf27dc37f92997b0f
iamminnyah <vcenxi@daum.net>
2024-03-12 18:55:57 +0900

    first commit

---
+++ ex13chap2.py
@@ -0,0 +1,7 @@
+circle_r = int(input('원의 반지름을 입력하세요 :'))
+PI = 3.141592
+circle_cc = (circle_r)*2*PI
+circle_ac = (circle_r)*(circle_r)*PI
+
+print('원의 둘레 =' ,circle_cc)
+print('원의 면적 =' ,circle_ac)
