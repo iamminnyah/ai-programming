70488c3c3d4713e128d80622d820d66ef30b979b
iamminnyah <vcenxi@daum.net>
2024-03-20 13:35:55 +0900

    first commit

---
+++ ex17chap3.py
@@ -0,0 +1,15 @@
+for i in range(3):
+    print('python')
+    print('is')
+    print('fun')
+
+
+for i in range(3):
+    print('python')
+    print('is')
+print('fun')
+
+for i in range(3):
+    print('python')
+print('is')
+print('fun')
